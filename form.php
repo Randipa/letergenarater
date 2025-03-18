69b72f74a20ccb128276d5e0664bd6beb8d872a5
Saliya Pathum <96324718+Randipa@users.noreply.github.com>
2025-03-18 13:58:22 +0530

    update

--- form.php
+++ form.php
@@ -66,15 +66,7 @@
         <!-- Navbar Links -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
+               
             </ul>
         </div>
     </div>
